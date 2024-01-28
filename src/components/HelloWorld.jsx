9641c55dabbb2532b135a47d709403666b982433
“richixws” <rwongsol@hotmail.com>
2024-01-28 16:02:35 -0500

    curso react componentes

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,16 @@
+
+export const  HelloWorld = ({user,id}) => {
+
+    //const name='Pepe';
+
+    return  (
+          <>
+                <div>hola mundo</div>
+                <h1>que tal {user} con el id {id}</h1>
+          </>
+
+    )
+    
+    
+
+}
