3024258f04da8b681c71ece357ac4d997a68298c
“richixws” <rwongsol@hotmail.com>
2024-01-28 21:25:50 -0500

    components y props

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,12 +1,14 @@
 
-export const  HelloWorld = ({user,id}) => {
+export const  HelloWorld = ({user,id, title='Hola Mundo'}) => {
 
     //const name='Pepe';
+    console.log(title);
 
     return  (
           <>
-                <div>hola mundo</div>
-                <h1>que tal {user} con el id {id}</h1>
+                <div>{title}</div>
+                <h1>{user.name} con edad {user.edad}</h1>
+                <h1>que tal {JSON.stringify(user)} con el id {id + 10}</h1>
           </>
 
     )
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { HelloWorld } from './components/HelloWorld'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorld user={'Pepe'} id={'1'}/>
+    <HelloWorld user={{name:'richard',edad:12}} id={1}/>
   </React.StrictMode>
 
 )
